908864528a1d7d5154e53bf719944d33a4f78d58
MorrisFreeman <morrisfreeman0718@gmail.com>
2022-05-29 09:34:22 +0900

    first client追加

---
+++ first-web-client/go.mod
@@ -0,0 +1,3 @@
+module first-web-client
+
+go 1.18
--- first-web-server/main.go
+++ first-web-server/main.go
@@ -9,30 +9,51 @@ import (
 )
 
 func main() {
-	fmt.Println("===サーバーを起動します===")
+	fmt.Println("=== サーバーを起動します ===")
 	listener, err := net.Listen("tcp", "localhost:8888")
 	if err != nil {
 		panic(err)
 	}
-	for {
-		fmt.Println("===クライアントからのアクセスを待っています...===")
-		conn, err := listener.Accept()
-		fmt.Println("===クライアントからのアクセスがありました！===")
-		if err != nil {
-			panic(err)
-		}
-		go func() {
-			fmt.Printf("Accept %v\n", conn.RemoteAddr())
-			request, err := http.ReadRequest(bufio.NewReader(conn))
-			if err != nil {
-				panic(err)
-			}
-			dump, err := httputil.DumpRequest(request, true)
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println(string(dump))
-			conn.Close()
-		}()
+	fmt.Println("=== クライアントからのアクセスを待っています... ===")
+	conn, err := listener.Accept()
+	fmt.Println("=== クライアントからのアクセスがありました！ ===")
+	// このタイミングではtcpのコネクションが接続されているだけ
+	// httpリクエストの読み込みはまだ
+	if err != nil {
+		panic(err)
 	}
+	fmt.Printf("Accept %v\n", conn.RemoteAddr())
+	request, err := http.ReadRequest(bufio.NewReader(conn))
+	if err != nil {
+		panic(err)
+	}
+	dump, err := httputil.DumpRequest(request, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dump))
+	fmt.Println("=== 接続を終了します ===")
+	conn.Close()
+	fmt.Println("=== サーバーを終了します ===")
+	// for {
+	// 	fmt.Println("=== クライアントからのアクセスを待っています... ===")
+	// 	conn, err := listener.Accept()
+	// 	fmt.Println("=== クライアントからのアクセスがありました！ ===")
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	go func() {
+	// 		fmt.Printf("Accept %v\n", conn.RemoteAddr())
+	// 		request, err := http.ReadRequest(bufio.NewReader(conn))
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		dump, err := httputil.DumpRequest(request, true)
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		fmt.Println(string(dump))
+	// 		conn.Close()
+	// 	}()
+	// }
 }
